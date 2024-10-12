2cb5fceaa03fcea3a0096d65f048637524896ca5
pacarti <a.paciorkowski@protonmail.com>
2024-10-13 00:41:58 +0200

    Added sorting regarding the job offers count - the technologies with the most count are displayed first, with the least count - last. Moreover, dynamic color assignment for each technology has been added.

--- juniorDeveloperTechsJJIT.py
+++ juniorDeveloperTechsJJIT.py
@@ -64,14 +64,28 @@ for address in addresses:
     techDict.setdefault(mo.group(1), int(mo.group(2)))
 
 
+colorsDict = {'Python': (0.16, 0.52, 0.8), 'Java': 'orange', 'PHP': 'purple', 'JS': (0.8, 0.67, 0), 'Ruby': 'red', 'AI/ML': 'gray', 'DevOps': 'cyan', 'Testing': (0.306, 0.561, 0.016), 'Mobile': '#2fff00'}
+
+# Create a techs reverse-sorted dictionary so that the one with most offers count is at the top:
+techDictSorted = dict(sorted(techDict.items(), key = lambda item: item[1], reverse=True))
+
+# Create a bar colors list with the proper technology assigned to it:
+colors = []
+
+for inLabel in techDictSorted.keys():
+    for techName, color in colorsDict.items():
+        if inLabel == techName:
+            colors.append(color)
+
+
 print("Job offers count:")
 
 for k, v in techDict.items():
     print(k + ':  ' + str(v))
 
-plt.bar(range(len(techDict)), list(techDict.values()), width = 0.7, color = [(0.16, 0.52, 0.8), 'orange', 'purple', (0.8, 0.67, 0), 'red', 'gray', 'cyan', (0.306, 0.561, 0.016), '#2fff00'])
+plt.bar(range(len(techDictSorted)), list(techDictSorted.values()), width = 0.7, color = colors)
 
-plt.xticks(range(len(techDict)), list(techDict.keys()))
+plt.xticks(range(len(techDictSorted)), list(techDictSorted.keys()))
 
 plt.title('justjoin.it - offers for Junior Developer')
 
