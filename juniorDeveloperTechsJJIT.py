d4e72d2138ffca7236f17f7d5eb3cddd9aa53614
pacarti <a.paciorkowski@protonmail.com>
2025-01-08 20:46:41 +0100

    Updated the script for the updates done one justjoin.it website -  new links & fixed the issue that the job count is not always loaded with looping. As a result of looping, the chart is being generated longer, but at least the script works again :)

--- juniorDeveloperTechsJJIT.py
+++ juniorDeveloperTechsJJIT.py
@@ -1,6 +1,20 @@
 # juniorDeveloperTechsJJIT.py - takes the junior job offers count for the following technologies: AI, Python, Ruby, Java, JS, PHP and DevOps and makes a bar chart from them
 
-import requests, bs4, re
+def fetchJobData(address):
+    try:
+        res = requests.get(address)
+        res.raise_for_status()
+
+        soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+        numOfJuniorJobsElem = soup.select('h1.MuiTypography-root')[0]
+
+        return numOfJuniorJobsElem
+
+    except IndexError:
+        return None
+
+import requests, bs4, re, time, datetime
 import matplotlib.pyplot as plt
 
 addresses = []
@@ -10,47 +24,47 @@ techAndNumRegex = re.compile(r'^(\w+\W?\w+)  - (\d{1,3})? offers$')
 techDict = {}
 
 # Python
-addressPy = 'https://justjoin.it/all-locations/python/experience-level_junior'
+addressPy = 'https://justjoin.it/job-offers/all-locations/python?experience-level=junior&orderBy=DESC&sortBy=published'
 addresses.append(addressPy)
 # Java
-addressJava = 'https://justjoin.it/all-locations/java/experience-level_junior'
+addressJava = 'https://justjoin.it/job-offers/all-locations/java?experience-level=junior&orderBy=DESC&sortBy=published'
 addresses.append(addressJava)
 # PHP
-addressPHP = 'https://justjoin.it/all-locations/php/experience-level_junior'
+addressPHP = 'https://justjoin.it/job-offers/all-locations/php?experience-level=junior&orderBy=DESC&sortBy=published'
 addresses.append(addressPHP)
 # JS
-addressJS = 'https://justjoin.it/all-locations/javascript/experience-level_junior'
+addressJS = 'https://justjoin.it/job-offers/all-locations/javascript?experience-level=junior&orderBy=DESC&sortBy=published'
 addresses.append(addressJS)
 # Ruby
-addressRuby = 'https://justjoin.it/all-locations/ruby/experience-level_junior'
+addressRuby = 'https://justjoin.it/job-offers/all-locations/ruby?experience-level=junior&orderBy=DESC&sortBy=published'
 addresses.append(addressRuby)
 # AI/ML
-addressAI = 'https://justjoin.it/all-locations/ai/experience-level_junior'
+addressAI = 'https://justjoin.it/job-offers/all-locations/ai?experience-level=junior&orderBy=DESC&sortBy=published'
 addresses.append(addressAI)
 # DevOps
-addressDevOps = 'https://justjoin.it/all-locations/devops/experience-level_junior'
+addressDevOps = 'https://justjoin.it/job-offers/all-locations/devops?experience-level=junior&orderBy=DESC&sortBy=published'
 addresses.append(addressDevOps)
 # Testing
-addressTesting = 'https://justjoin.it/all-locations/testing/experience-level_junior'
+addressTesting = 'https://justjoin.it/job-offers/all-locations/testing?experience-level=junior&orderBy=DESC&sortBy=published'
 addresses.append(addressTesting)
 # Mobile
-addressTesting = 'https://justjoin.it/all-locations/mobile/experience-level_junior'
+addressTesting = 'https://justjoin.it/job-offers/all-locations/mobile?experience-level=junior&orderBy=DESC&sortBy=published'
 addresses.append(addressTesting)
 
 
 for address in addresses:
-    try:
-        res = requests.get(address)
-    except ConnectionError:
-        print('Connection error!')
-        exit()
-
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
-
-    numOfJuniorJobsElem = soup.select('h1.MuiTypography-root')[0]
+    # while True:
+        
 
     # this doesn't exist in HTML thus is not saved into the list - it is dynamically generated:
     # numOfJuniorJobs = pythonSoup.select('.MuiTab-iconWrapper.css-1604j7q')
+
+
+    numOfJuniorJobsElem = fetchJobData(address)
+
+    # Since the h1 element containing the job offers count is not always loaded, then numOfJuniorJobsElem will be None. Then loop the function until it will be loaded properly:
+    while numOfJuniorJobsElem == None:
+        numOfJuniorJobsElem = fetchJobData(address)
 
 
     textNumOfJuniorJobs = numOfJuniorJobsElem.getText()
@@ -80,14 +94,16 @@ for inLabel in techDictSorted.keys():
 
 print("Job offers count:")
 
-for k, v in techDict.items():
+for k, v in techDictSorted.items():
     print(k + ':  ' + str(v))
 
 plt.bar(range(len(techDictSorted)), list(techDictSorted.values()), width = 0.7, color = colors)
 
 plt.xticks(range(len(techDictSorted)), list(techDictSorted.keys()))
 
-plt.title('justjoin.it - offers for Junior Developer')
+dt = datetime.datetime.now()
+
+plt.title('justjoin.it - offers for Junior Developer - ' + dt.strftime('%d/%m/%Y'))
 
 plt.xlabel('Technology')
 
